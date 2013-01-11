9df851552076e3d83ba8e0a06b8740e2291afb8f
rtucker@mozilla.com <rtucker@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2013-01-11 11:11:32 +0000

    Added google analytics header per bug# 829264
    git-svn-id: http://svn.mozilla.org/projects/phonebook/trunk@112125 4eb1ac78-321c-0410-a911-ec516a8615a5

--- templates/header.php
+++ templates/header.php
@@ -7,6 +7,17 @@
     <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico" />
     <script type="text/javascript" src="js/prototype.js"></script>
     <script type="text/javascript" src="js/common.js"></script>
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-35433268-20']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
   </head>
 
 <body>
