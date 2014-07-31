1e1df8854c1678f7007409520a7cb560132a2364
rtucker@mozilla.com <rtucker@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2014-07-31 12:55:11 +0000

    Added font-awesome for icons
    git-svn-id: http://svn.mozilla.org/projects/phonebook/trunk@130686 4eb1ac78-321c-0410-a911-ec516a8615a5

--- templates/header.php
+++ templates/header.php
@@ -3,7 +3,9 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
     <title>Mozilla Phonebook</title>
+    <link href="./css/font-awesome.min.css" rel="stylesheet" type="text/css" />
     <link href="css/style.css" rel="stylesheet" type="text/css" />
+
     <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico" />
     <script type="text/javascript" src="js/prototype.js"></script>
     <script type="text/javascript" src="js/common.js"></script>
