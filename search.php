19461d3b12660f21fda84a1d28c2aa333eab101f
byron jones <byron@glob.com.au>
2016-08-26 14:36:13 +0800

    Fix handling of search limits.
    - handle limit checking and truncation directly instead for greater
      control
    - generate "showing only the first N of Y results" html in the html
      output only

--- search.php
+++ search.php
@@ -7,11 +7,12 @@ $auth = new MozillaAuthAdapter();
 $search = new MozillaSearchAdapter($ldapconn);
 $keyword = isset($_GET["query"]) ? $_GET["query"] : '';
 $exact = isset($_GET["exact_search"]) ? true : false;
-$entries = normalize($search->search_users($keyword, $exact=$exact));
+$search_result = $search->search_users($keyword, $exact=$exact);
+$search_result['users'] = normalize($search_result['users']);
 $attr_preps = get_attr_preprocessors();
 
 $preprocess_attr_functions = array();
-foreach ($entries as &$entry) {
+foreach ($search_result['users'] as &$entry) {
   foreach ($entry as $name => $attribute) {
     $prep = isset($attr_preps[$name]) ? $attr_preps[$name] : NULL;
     if (!isset($preprocess_attr_functions[$prep])) {
@@ -31,4 +32,4 @@ if (!in_array($format, $output_formats) || !file_exists("output-$format.inc")) {
 require_once("output-$format.inc");
 $function = "output_$format";
 $dn = $auth->user_to_dn($_SERVER["REMOTE_USER"]);
-call_user_func($function, $entries, $auth->is_phonebook_admin($ldapconn, $dn));
+call_user_func($function, $search_result, $auth->is_phonebook_admin($ldapconn, $dn));
