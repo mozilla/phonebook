e7bb9571faae788f31440ea95f3a67771867e1e7
Richard Soderberg <rsoderberg@gmail.com>
2017-05-05 22:12:40 -0700

    upgrade jquery from 3.1.0 to 3.2.1

--- config-srihashes.php
+++ config-srihashes.php
@@ -3,7 +3,7 @@
 $GLOBALS["SRIHASHES"] = array(
 "css/font-awesome.min.css" => "sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN",
 "css/style.css" => "sha384-mgy1BxWQnzpdmyXWef36g4C0ZiptOfSHpNpX28iA/zhX08L4nlxErijaqvJsqTFh",
-"js/jquery-min.js" => "sha384-nrOSfDHtoPMzJHjVTdCopGqIqeYETSXhZDFyniQ8ZHcVy08QesyHcnOUpMpqnmWq",
+"js/jquery-min.js" => "sha384-xBuQ/xzmlsLoJpyjoggmTEz8OWUFM0/RC5BsqQBDX2v5cMvDHcMakNTNrHIW2I5f",
 "js/phonebook.js" => "sha384-br584pnoJl5MMYZToUmcjk0tTj5Zcjwq9WafKRZUuUyTDzlWCGuuctYITZeYQxAW",
 );
 ?>
