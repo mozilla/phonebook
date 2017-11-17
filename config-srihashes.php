8a4aade5e1fb36551a144466648568118a983e28
Richard Soderberg <rsoderberg@gmail.com>
2017-11-16 16:50:56 -0800

    repair handling of filter views [#1362630]

--- config-srihashes.php
+++ config-srihashes.php
@@ -4,6 +4,6 @@ $GLOBALS["SRIHASHES"] = array(
 "css/font-awesome.min.css" => "sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN",
 "css/style.css" => "sha384-3+qaYNVG3XTPDSYGxgT9IhtdO60ELHYGV2E8VQ2sqVHOaIDe0iThsTeqyJnm6VxK",
 "js/jquery-min.js" => "sha384-xBuQ/xzmlsLoJpyjoggmTEz8OWUFM0/RC5BsqQBDX2v5cMvDHcMakNTNrHIW2I5f",
-"js/phonebook.js" => "sha384-OHOQgREvSvnUyybCf6lJarobcZSqJcfLNbHQbjutMb3rja7ChTPgzTo86rQqCFaZ",
+"js/phonebook.js" => "sha384-LrlUJZhEeTEgl+4p9m0i01uIvFHdaaPp+mks6Rz2qcruKnPI027l84KXc5PkCqGG",
 );
 ?>
